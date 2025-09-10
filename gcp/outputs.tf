a52c997ce07803fa57705f8734aad00762f94c1c
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-09-09 17:40:20 -0700

    Update outputs.tf

--- gcp/outputs.tf
+++ gcp/outputs.tf
@@ -1,3 +1,7 @@
 output "public_ip" {
-  value = google_compute_instance.ssh_vm.network_interface.0.access_config.0.nat_ip
+  value = google_compute_instance.super_cool_gce.network_interface.0.access_config.0.nat_ip
+}
+
+output "machine_uri" {
+  value = google_compute_instance.super_cool_gce.self_link
 }
